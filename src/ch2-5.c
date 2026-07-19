bbf5f376aef2f879507d66cddb31210662a70cfc
Skyclad1004 <kimminstar12@gmail.com>
2026-07-19 22:18:04 +0900

    Ch02-5: Integer Constants in Three Number Systems

---
+++ src/ch2-5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d\n", 12); // print a decimal integer constant
+	printf("%d\n", 014); // print a octal integer constant
+	printf("%d\n", 0xc); // print a hex integer constant
+
+	return 0;
+}
