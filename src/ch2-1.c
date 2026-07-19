303cfa2878f92eec313fa1c8b06c0809bbe8a4c6
Skyclad1004 <kimminstar12@gmail.com>
2026-07-19 15:54:07 +0900

    Ch02-1

---
+++ src/ch2-1.c
@@ -0,0 +1,13 @@
+﻿'HonGong.exe'(Win32): 'C:\studyC\HonGong\x64\Debug\HonGong.exe'을(를) 로드했습니다. 기호가 로드되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\ntdll.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\kernel32.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\KernelBase.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\apphelp.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\vcruntime140d.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\ucrtbased.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+8284 스레드가 종료되었습니다(코드: 0 (0x0)).
+'HonGong.exe'(Win32): 'C:\Windows\System32\kernel.appcore.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+'HonGong.exe'(Win32): 'C:\Windows\System32\msvcrt.dll'을(를) 로드했습니다. 포함/제외 설정으로 기호 로드가 비활성화되었습니다.
+16392 스레드가 종료되었습니다(코드: 0 (0x0)).
+2764 스레드가 종료되었습니다(코드: 0 (0x0)).
+'[19372] HonGong.exe' 프로그램이 종료되었습니다(코드: 0 (0x0)).
