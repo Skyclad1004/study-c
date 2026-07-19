2f561b86fd079d3203e7058edd838787886fc212
Skyclad1004 <kimminstar12@gmail.com>
2026-07-19 17:21:18 +0900

    Ch02-2: Program to Print a String

---
+++ src/ch2-2.c
@@ -0,0 +1,9 @@
+# include <stdio.h> //copy content of 'stdio.h' into the program
+
+int main(void)
+{
+	printf("Be happy"); // print str "Be happy"
+	printf("My friend"); // print str "My friend
+
+	return 0;
+}
