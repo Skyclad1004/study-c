0236ed193ba89f1571737757f50b2e3a579a36e7
Skyclad1004 <kimminstar12@gmail.com>
2026-07-24 02:05:48 +0900

    Ch03-9: Keyboard Input Using scanf

---
+++ src/ch3-9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	scanf("%d", &a); // enter a value into variable a here
+	printf("entered value : %d\n", a); // print the entered value
+
+	return 0;
+}
