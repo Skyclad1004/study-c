82945b12e2ffa8daad8c9351bf23adf96fc47ad1
Skyclad1004 <kimminstar12@gmail.com>
2026-07-24 21:48:33 +0900

    Ch04-3: Increment and Decrement Operators

---
+++ src/ch4-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 10;
+
+	++a; // increase the variable's value by 1
+	--b; // decrease the variable's value by 1
+
+	printf("a : %d\n", a);
+	printf("b : %d\n", b);
+
+	return 0;
+}
