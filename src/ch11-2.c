e9835ae476bd305f9d74552039e06f124b487fe3
Skyclad1004 <kimminstar12@gmail.com>
2026-08-14 23:15:32 +0900

    Ch11-2: Entering Whitespace or Control Characters

---
+++ src/ch11-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch1, ch2;
+
+	scanf("%c%c", &ch1, &ch2); // enter two characters in a row
+	printf("[%c%c]", ch1, ch2); // print the entered characters
+
+	return 0;
+}
